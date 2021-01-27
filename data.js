fa71d52154b652d95a9d7e51faa8f87941daf673
Samantha Solomon <sleahsolomon@gmail.com>
2021-01-27 15:02:43 -0500

    lab

---
+++ data.js
@@ -0,0 +1,84 @@
+{
+    "name" : "Sam",
+    "food" : "noodles",
+    "age" : "34",
+    "OS" : "MacOS Catalina",
+    "hobbies" : ["destroying the patriarchy", "crossfit", "reading"]
+},
+{
+    "name" : "Sean",
+    "food" : "Curry",
+    "age" : 29,
+    "OS" : "Mac",
+    "hobbies" : ["skiing", "hiking", "gaming"]
+},
+{
+    "name" : "Clayton",
+    "food" :  "ramen",
+    "OS" : "Linux",
+    "hobbies" : ["videogames", "reading"],
+    "age" : 25
+},
+{
+    "name" : "Anny",
+    "food" : "Sushi",
+    "OS" : "Mac",
+    "hobbies" : ["Long walks", "hiking", "drawing"],
+    "age" : 28
+} ,
+{
+    "name" : "Kel",
+    "food" : "Tacos",
+    "age" : 33,
+    "OS" : "Mac",
+    "hobbies" : ["Guitar", "Live Music", "TV"]
+},
+{
+    "name" : "Jorge",
+    "food": "Ramen",
+    "age" : 41,
+    "OS" : "MacOS",
+    "hobbies" : "Dance"
+},
+{
+    "name" : "Kaung",
+    "food" : "Tacos",
+    "age" : 24,
+    "OS": "Xubuntu",
+    "hobbies" : ["Keyboards", "Art", "People-watching"]
+},
+{
+    "name" : "Troy",
+    "food": "Fried Chicken",
+    "age" : 27,
+    "OS": "Mac Big Sur",
+    "hobbies" : ["skateboarding", "martial arts"]
+},
+{
+    "name" : "Gary",
+    "food" : "pizza",
+    "OS" : "Mac",
+    "hobbies" : ["photography"],
+    "age" : 31
+},
+{
+    "name" : "Alex",
+    "food": "Pasta Carbonara",
+    "age" : 26,
+    "OS" : "MacOS",
+    "Hobbies" : ["soccer", "gaming", "yoga", "gym"]
+},
+{
+    "name": "Adrian Mendez",
+    "food": "sushi",
+    "age": 20,
+    "OS" : "mac",
+    "hobbies": ["running", "sleeping"]
+},
+{
+    "name" : "Andrew",
+    "food": "Steak",
+    "age" : 31,
+    "OS": "MacOS",
+    "hobbies": ["Music", "Swimming", "Workout", "Running"]
+}
